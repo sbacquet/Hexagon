a2574c4ab4d550c951611a967fbb33235a49d301
Sebastien Bacquet <sebastien.bacquet@gmail.com>
2018-02-11 11:33:33 +0100

    Introduced PowershellScriptExecutor

---
+++ Hexagon.Tests/PowershellScriptExecutorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Hexagon.Tests
+{
+    [TestClass]
+    public class PowershellScriptExecutorTests
+    {
+        [TestMethod]
+        public void Test1()
+        {
+            var ps = new PowershellScriptExecutor();
+            string script = @"param($param1) ""param1 = $param1""";
+            var outputs = ps.Execute(script, ("param1", 3));
+            Assert.AreEqual("param1 = 3", outputs.First());
+        }
+    }
+}
---
+++ Hexagon/PowershellScriptExecutor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Collections.ObjectModel;
+
+namespace Hexagon
+{
+    public class PowershellScriptExecutor
+    {
+        public PowershellScriptExecutor()
+        {
+        }
+
+        public IEnumerable<object> Execute(string script, params (string, object)[] parameters)
+        {
+            using (PowerShell powerShellInstance = PowerShell.Create())
+            {
+                // use "AddScript" to add the contents of a script file to the end of the execution pipeline.
+                // use "AddCommand" to add individual commands/cmdlets to the end of the execution pipeline.
+                powerShellInstance.AddScript(script);
+
+                // use "AddParameter" to add a single parameter to the last command/script on the pipeline.
+                powerShellInstance.AddParameters(parameters.ToDictionary(nameAndValue => nameAndValue.Item1, nameAndValue => nameAndValue.Item2));
+
+                Collection<PSObject> psOutput = powerShellInstance.Invoke();
+                return psOutput.Select(output => output.BaseObject);
+            }
+        }
+    }
+}
